74cc6b55ceb72956012207db6e4d4238e8dc21fa
tokiya.horikawa <ayikot4423@gmail.com>
2022-05-01 23:04:01 +0900

    chore: Settings storybook for nextjs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import "../styles/globals.css";
+import * as NextImage from "next/image";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +10,10 @@ export const parameters = {
     },
   },
 };
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
