7dfc1a2c0c0112eb1c8243edc487864b141c5968
tokiya.horikawa <ayikot4423@gmail.com>
2022-05-01 19:51:19 +0900

    feat: Update style and date format

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,6 +1,8 @@
 import { GetStaticPropsContext } from "next";
 import Head from "next/head";
+import utilStyles from "../../styles/utils.module.css";
 import Layout from "../../components/layout";
+import Date from "../../components/date";
 import { getAllPostIds, getPostData, Data } from "../../lib/posts";
 
 type Props = { postData: Data };
@@ -11,13 +13,13 @@ export default function Post({ postData }: Props) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
