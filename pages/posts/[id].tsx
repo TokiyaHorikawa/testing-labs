3c18aa11bb29138cbf4b850ebf184610bd720da0
tokiya.horikawa <ayikot4423@gmail.com>
2022-05-01 18:56:05 +0900

    feat: Create Dynamic route

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,37 @@
+import { GetStaticPropsContext } from "next";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData, Data } from "../../lib/posts";
+
+type Props = { postData: Data };
+
+export default function Post({ postData }: Props) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+  const postData =
+    typeof params?.id === "string" ? await getPostData(params.id) : null;
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
