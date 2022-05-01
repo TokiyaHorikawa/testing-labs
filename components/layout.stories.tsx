29561524b3e8108eac8d300aed313820600e5175
tokiya.horikawa <ayikot4423@gmail.com>
2022-05-01 21:17:00 +0900

    chore: Change setting of storybook main.js

---
+++ components/layout.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "./layout";
+
+export default {
+  component: Layout,
+  title: "components/Layout",
+};
+
+export const Default = {
+  args: { children: <h1>Hello Next.js</h1>, home: true },
+};
