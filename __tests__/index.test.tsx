1064b164a8db4ddd4d66c76ad378f3caf7086b4b
tokiya.horikawa <ayikot4423@gmail.com>
2022-05-04 19:44:47 +0900

    chore: Add test liblary

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  const allPostsData = [{ id: "id", date: "2022" }];
+  it("renders a heading", () => {
+    render(<Home allPostsData={allPostsData} />);
+
+    const heading = screen.getByRole("heading", {
+      name: /Next\.js Blog/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
